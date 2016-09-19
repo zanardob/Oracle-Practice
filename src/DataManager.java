87223867b5e5511cf5054cffc27231136d46fd10
Lucas Silveira de Moura <lucasmour4@gmail.com>
2016-09-19 18:29:26 -0300

    :book: Ordinary tables display fine

--- src/DataManager.java
+++ src/DataManager.java
@@ -7,18 +7,42 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class DataManager {
+
     public ObservableList<String> getTableNames() throws ClassNotFoundException, SQLException {
         Connection connection = DatabaseConnector.getConnection();
         Statement statement = connection.createStatement();
         ObservableList<String> names = FXCollections.observableArrayList();
-        String query = "SELECT table_name FROM user_tables MINUS SELECT table_name FROM user_snapshots";
+        String query;
+        ResultSet rs;
 
-        ResultSet rs = statement.executeQuery(query);
+        query = "SELECT table_name FROM user_tables MINUS SELECT table_name FROM user_snapshots";
+        rs = statement.executeQuery(query);
         while(rs.next()) {
             names.add(rs.getString("table_name"));
         }
-        System.out.println(names);
+
+        query = "SELECT view_name FROM user_views";
+        rs = statement.executeQuery(query);
+        while(rs.next()) {
+            names.add(rs.getString("view_name") + " (view)");
+        }
+
+        query = "SELECT table_name FROM user_snapshots";
+        rs = statement.executeQuery(query);
+        while(rs.next()) {
+            names.add(rs.getString("table_name") + " (snapshot)");
+        }
 
         return names;
+    }
+
+    public ResultSet getTable(String tableName) throws SQLException, ClassNotFoundException {
+        Connection connection = DatabaseConnector.getConnection();
+        Statement statement = connection.createStatement();
+        ObservableList<String> names = FXCollections.observableArrayList();
+        String query = "SELECT * FROM " + tableName;
+        ResultSet rs = statement.executeQuery(query);
+
+        return rs;
     }
 }
--- src/MainController.java
+++ src/MainController.java
@@ -1,9 +1,15 @@
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
-
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.*;
+import javafx.util.Callback;
+import javax.xml.transform.Result;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import javafx.scene.control.TableColumn.CellDataFeatures;
 
 public class MainController {
     private DataManager dm;
@@ -20,7 +26,47 @@ public class MainController {
             txtError.setText("Check your JDBC driver.");
             e.printStackTrace();
         } catch (SQLException e) {
-            txtError.setText("Connection failed.");
+            txtError.setText("Erro SQL: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    void fillTable() {
+        ResultSet rs;
+        ObservableList<ObservableList> data = FXCollections.observableArrayList();
+        table.getItems().clear();
+        table.getColumns().clear();
+
+        try {
+            rs = dm.getTable(cbox.getValue());
+            for(int i = 0; i < rs.getMetaData().getColumnCount(); i++) {
+                final int j = i;
+                TableColumn column = new TableColumn(rs.getMetaData().getColumnName(i + 1));
+                column.setCellValueFactory(new Callback<CellDataFeatures<ObservableList, String>, ObservableValue<String>>() {
+                    @Override
+                    public ObservableValue<String> call(CellDataFeatures<ObservableList, String> param) {
+                        return new SimpleStringProperty(param.getValue().get(j).toString());
+                    }
+                });
+
+                table.getColumns().addAll(column);
+            }
+
+            while(rs.next()) {
+                ObservableList<String> row = FXCollections.observableArrayList();
+                for(int i = 1; i <= rs.getMetaData().getColumnCount(); i++) {
+                    row.add(rs.getString(i));
+                }
+                data.add(row);
+            }
+
+            table.setItems(data);
+
+        } catch (ClassNotFoundException e) {
+            txtError.setText("Check your JDBC driver.");
+            e.printStackTrace();
+        } catch (SQLException e) {
+            txtError.setText("Erro SQL: " + e.getMessage());
             e.printStackTrace();
         }
     }
@@ -30,6 +76,8 @@ public class MainController {
         this.table = table;
         this.btnInsert = btnInsert;
         this.txtError = txtError;
+
+        cbox.setOnAction(event -> fillTable());
 
         dm = new DataManager();
     }
